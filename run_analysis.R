3b1ae75adbc9e2c10dbdba99674bcc246ce1373c
Toby Wright <49879024+tobyjameswright@users.noreply.github.com>
2019-06-10 07:17:33 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -8,7 +8,7 @@ test_activity <- read.table("UCI HAR Dataset/test/y_test.txt")
 activity_label <- read.table("UCI HAR Dataset/activity_labels.txt")
 features <- read.table("UCI HAR Dataset/features.txt")
 subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
-subject_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
 
 #merge data sets
 data <- rbind(test, train)
